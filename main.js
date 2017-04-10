38fa7937b5d19843745576c9745926e46b501470
Pontus <Akira@Pontuss-MacBook-Pro.local>
2017-04-10 10:58:09 +0200

    Added some minor JS fixed css

--- main.js
+++ main.js
@@ -6,4 +6,27 @@ function main(){
 });
  }
 
+ function pageswitch(){
+    $(".aboutdiv").hide();
+    $(".contactdiv").hide();
+
+     $("#nav1").on("click", function(){
+       $(".aboutdiv").hide();
+       $(".contactdiv").hide();
+       $(".postdiv").show();
+    });
+     $("#nav2").on("click", function(){
+       $(".contactdiv").hide();
+       $(".postdiv").hide();
+       $(".aboutdiv").show();
+    });
+    $("#nav3").on("click", function(){
+      $(".aboutdiv").hide();
+      $(".postdiv").hide();
+      $(".contactdiv").show();
+    });
+ }
+
+
 $(document).ready(main);
+$(document).ready(pageswitch);
